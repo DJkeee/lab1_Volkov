02df5d9ad4e4c7b8cc81d4ad0b50c6dc5ac19eed
DJkee <160426238+DJkeee@users.noreply.github.com>
2024-03-12 14:47:52 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,5 @@
 #include "test.h"
 
-/*void printsubstring(String *s) ;
-void initStringStruct(String *s, char *initialStr) ;
-void freeStringStruct(String *s) ;
-void printString(String *s) ;
-void append_string(String *s, char *new_s) ;
-void searchSubstring(String *text, String *pattern) ;
-void searchSubstring_nocase(String *text, String *pattern) ;
-char *createSubstring(String *str, int i, int j) ; */
-
 void string_test_standart(){
     String test_str ;
     char* test_input = "DOCTOR" ;
